8bc61a44a55b12e279834eb1ce44b8e39e6a2556
AndaChain <andaanda789@gmail.com>
2021-10-13 10:19:52 +0700

    ui_1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+	before_action :set_init
+	def set_init
+		@current_user = true
+	end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'manage/index'
+  get 'infor/index'
+  get 'login/user'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'manage#index'
 end
