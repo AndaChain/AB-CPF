698f3a23ca8fd2f5d05272f929c62dfb5f545ef5
Taweesin6201012620295 <s6201012620295@email.kmutnb.ac.th>
2021-10-13 20:14:26 +0700

    login & Department UI

--- app/controllers/manage_controller.rb
+++ app/controllers/manage_controller.rb
@@ -1,11 +1,7 @@
 class ManageController < ApplicationController
-before_action :login_first
-	def login_first
-		if !@current_user
-			redirect_to login_user_path
-		end
-	end
-	
 	def index
+	end
+
+	def shifttime
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'manage/index'
-  get 'infor/index'
-  get 'login/user'
+  get 'manage/' ,to: "manage#index"
+  get 'infor/' ,to: "infor#index"
+  get 'login/' ,to: "login#user"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'login#user'
+  
+  namespace :manage do
+    resources :shifttime
+  end
 end
