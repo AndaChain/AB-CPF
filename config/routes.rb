ce1f767d9d125cc976164c272a8144837797ce94
Taweesin6201012620295 <s6201012620295@email.kmutnb.ac.th>
2021-10-15 21:15:07 +0700

    Login Clear

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'infor/' ,to: "infor#index"
   get 'login/' ,to: "login#user"
+  post 'login' => 'login#submit'
+  #get '/login', :controller => 'login', :action => 'submit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'login#user'
   
