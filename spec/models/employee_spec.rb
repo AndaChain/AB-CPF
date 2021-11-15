48e44190cb9fdd2ee4d20ce21a1c26634721be78
panat54083 <panat54083@gmail.com>
2021-11-15 10:23:53 -0800

    TDD. Password must not be nill

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -3,18 +3,25 @@ require 'rails_helper'
 RSpec.describe Employee, type: :model do
     current_user = Employee.find_or_create_by!(id_e: 'e1', password: '1230',code:'d1')
     it 'has a id and it is not nil' do
-    employee_bot =  Employee.new(
-        id_e: '',
-        password: '1230'
+        employee_bot =  Employee.new(
+            id_e: '',
+            password: '1230'
+        )
+        expect(employee_bot).to_not be_valid
 
-    )
-
-    expect(employee_bot).to_not be_valid
-    employee_bot.id_e = 'e_bot'
-    expect(employee_bot).to be_valid
+        employee_bot.id_e = 'e_bot'
+        expect(employee_bot).to be_valid
     end
 
     it 'has a password and it is not nil' do
+        employee_bot =  Employee.new(
+            id_e: 'm1',
+            password: ''
+        )
+        expect(employee_bot).to_not be_valid
+
+        employee_bot.password = '1230'
+        expect(employee_bot).to be_valid
     end
 
 
