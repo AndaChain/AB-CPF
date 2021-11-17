29edb9de7530bb8d1f85c7e98df82bdcbae5ab69
panat54083 <panat54083@gmail.com>
2021-11-16 22:34:49 -0800

    Create Simple Test

--- spec/controller/sessions_controller_spec.rb
+++ spec/controller/sessions_controller_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.feature "login", type: :feature do
+    before(:each) do
+        Department.new(code: 'd1', name: 'Chicken', parent_code: 'd1').save!
+        Manager.new(id_m: 'm1', password: '1230', first: 'Manager1', last: 'Last_Manager1', parent_code: "d1").save!
+        Employee.new(id_e: "e1", password: "1230", first: "Employee1", last: "Last_Employee1", code: "d1").save!
+
+    end
     describe 'login page' do
         it 'has UserID and password text' do
             visit login_path
@@ -9,14 +15,24 @@ RSpec.feature "login", type: :feature do
             expect(page).to have_content('Password')
         end
 
-        it 'can put username and password' do
+        it 'shoud go to manager page if I am manager' do
             visit login_path
             within('form') do
                 fill_in 'user_name', with: 'm1'
                 fill_in 'password', with: '1230'
             end
             click_button 'Log in'
-            expect(page).to have_content('Welcome to Time Attendance')
+            expect(current_path).to eql('/manage')
+        end
+
+        it 'shoud go to information page if I am employee' do
+            visit login_path
+            within('form') do
+                fill_in 'user_name', with: 'e1'
+                fill_in 'password', with: '1230'
+            end
+            click_button 'Log in'
+            expect(current_path).to eql('/infor')
         end
 
     end
