7abf37f75cf9b274c3e2dc083f795cbde5ca0624
panat54083 <panat54083@gmail.com>
2021-11-16 22:05:38 -0800

    TDD it have userID and Password text

--- spec/controller/sessions_controller_spec.rb
+++ spec/controller/sessions_controller_spec.rb
@@ -2,7 +2,14 @@ require 'rails_helper'
 
 RSpec.feature "login", type: :feature do
     describe 'login page' do
-        it 'shows the right content' do
+        it 'has UserID and password text' do
+            visit login_path
+            sleep(1)
+            expect(page).to have_content('UserID')
+            expect(page).to have_content('Password')
+        end
+
+        it 'can put username and password' do
             visit login_path
             within('form') do
                 fill_in 'user_name', with: 'm1'
@@ -11,5 +18,6 @@ RSpec.feature "login", type: :feature do
             click_button 'Log in'
             expect(page).to have_content('Welcome to Time Attendance')
         end
+
     end
 end
