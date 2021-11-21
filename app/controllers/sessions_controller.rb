3ab9c31cf3191c3a55052c3ef9a6d64553666ce1
Taweesin6201012620295 <68320677+Taweesin6201012620295@users.noreply.github.com>
2021-11-21 21:31:15 +0700

    alert error v1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,17 +2,22 @@ class SessionsController < ApplicationController
     def create
         user_id = params[:user_name]
         pass = params[:password]
-    
+        
         check_user_manager = Manager.find_by(id_m: user_id ,password: pass)
         check_user_employee = Employee.find_by(id_e: user_id ,password: pass)
 
+        flash[:error] = 'Password Incorrect' 
+        flash[:alert] = 'Please enter your UID or password'
+        
         if (check_user_manager == nil) & (check_user_employee == nil)
-            flash[:warning] = 'Password incorrect'
+            
             redirect_to login_path
+
           elsif check_user_manager == nil
             session[:user_id] = check_user_employee.id_e
             session[:password] = check_user_employee.password
             redirect_to infor_path
+
           else
             session[:user_id] = check_user_manager.id_m
             session[:password] = check_user_manager.password
