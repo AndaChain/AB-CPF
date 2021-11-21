f6c9631a7184bb387126ea2fdffdbc97b3f6d255
AndaChain <andaanda789@gmail.com>
2021-11-21 22:46:35 +0700

    just commit before pull 1

--- app/controllers/manage_controller.rb
+++ app/controllers/manage_controller.rb
@@ -26,10 +26,10 @@ class ManageController < ApplicationController
 
             @check_enter = {}
             @all_emp.each do |emp|
-                if  emp.time_recodes[0].end_actual - emp.time_recodes[0].start_actual < 0
-                    @check_enter[emp.id_e] = false
-                else
+                if  (emp.time_recodes[0].start_actual != nil) && (emp.time_recodes[0].end_actual == nil)
                     @check_enter[emp.id_e] = true
+                else
+                    @check_enter[emp.id_e] = false
                 end
                 
             end
