45cb7ebaa381c004c956a611be1319004bbcff0e
AndaChain <andaanda789@gmail.com>
2021-11-23 03:48:41 +0700

    shift time future 2 but still bug, na!

--- app/controllers/manage_controller.rb
+++ app/controllers/manage_controller.rb
@@ -38,7 +38,7 @@ class ManageController < ApplicationController
                 
             end
             @check_enter = @check_enter.to_json
-            @hash = Employee.all[0].keep_shifts[0].keep_shift[DateTime.parse(params[:format])]
+            #@hash = Employee.all[0].keep_shifts[0].keep_shift[DateTime.parse(params[:format])]
 			@date = params[:format]
         rescue
             redirect_to login_path
@@ -59,6 +59,7 @@ class ManageController < ApplicationController
 				Employee.find_by(id_e: id_e).update(ot_plan: 0) # if input is null, it will be 0 hr.
 			end
         end
+        check_date_update(params[:date])
     end
 
     def create
@@ -84,6 +85,7 @@ class ManageController < ApplicationController
 					end
 				end
         end
+        check_date_update(params[:date])
     end
     
     def destroy
@@ -91,75 +93,162 @@ class ManageController < ApplicationController
 		puts "***************************destroy***************************"
         puts params
         Employee.find_by(id_e: params[:id_e]).update(shifter_code: nil)
+        check_date_update(params[:format])
         redirect_to manage_path(id: params[:id])
         #redirect_to :back
 		#redirect_back(fallback_location: manage_path(id: params[:id]))
     end
     
-    def check_date(date)
+    def check_date(select_date)
+				Employee.all.each do |emp|
+					if emp.keep_shifts == []
+						# new one will create empty infor
+						emp.keep_shifts.create!(keep_shift: {select_date => [nil,0]})
+					end
+					begin
+								# Who has infor in that date will not go to "rescue NoMethodError"
+																	
+								hash_exist = emp.keep_shifts[0].keep_shift  # this is hash
+																	
+								#take hash in that select date to update them or create new one
+								shifter_code_keep = hash_exist[select_date][0] 
+								ot_plan_keep = hash_exist[select_date][1]
+							
+								emp.update(shifter_code: shifter_code_keep, ot_plan: ot_plan_keep)
+					rescue NoMethodError
+								# not found date that select from calendar
+								hash_temp = emp.keep_shifts[0].keep_shift
+								hash_temp[select_date] = [nil,0]
+								emp.keep_shifts[0].update(keep_shift: hash_temp)
+					end
+				end
+=begin
 		puts "START@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 		puts params
 		select_date = date
 		Employee.all.each do |emp|
 						begin
-							if emp.keep_shifts == []
-								# new one will create empty infor
-								emp.keep_shifts.create!(keep_shift: {date => [nil,0]})
-							end
-							# Who has infor in that date will not go to "rescue NoMethodError"
-							
-							hash_exist = emp.keep_shifts[0].keep_shift  # this is hash
-							
-							#take hash in that select date to update them or create new one
-							shifter_code_keep = hash_exist[select_date][0] 
-							ot_plan_keep = hash_exist[select_date][1]
-							
-							shifter_code = emp.shifter_code
-							ot_plan = emp.ot_plan
-							
-							change_ot = 0
-							change_shifter = ''
-							
-							puts "#{shifter_code_keep}==#{shifter_code}==#{ot_plan_keep}==#{ot_plan}"
-							
-							#XY = shifter_code case|ot case
-							if( (shifter_code != shifter_code_keep) && (ot_plan != ot_plan_keep) ) # 00
-								hash_exist[select_date] = [shifter_code, ot_plan]
-								puts "00"
-								change_ot = ot_plan
-								change_shifter = shifter_code
-								
-							elsif ( (shifter_code != shifter_code_keep) && (ot_plan == ot_plan_keep) )#01
-								hash_exist[select_date] = [shifter_code, ot_plan]
-								puts "01"
-								change_ot = ot_plan_keep
-								change_shifter = shifter_code
-								
-							elsif ( (shifter_code == shifter_code_keep) && (ot_plan != ot_plan_keep) ) #10
-								hash_exist[select_date] = [shifter_code, ot_plan]
-								puts "10"
-								change_ot = ot_plan
-								change_shifter = shifter_code_keep
-								
-							elsif ( (shifter_code == shifter_code_keep) && (ot_plan == ot_plan_keep) ) #11
-								hash_exist[select_date] = [shifter_code, ot_plan]
-								puts "11"
-								change_ot = ot_plan_keep
-								change_shifter = shifter_code_keep
-								
-							end
-							
-							puts "================== shifter_code=>#{change_shifter}, ot_plan=>#{change_ot} =================="
-							emp.keep_shifts[0].update(keep_shift: hash_exist)
-							emp.update(shifter_code: change_shifter, ot_plan: change_ot)
+									if emp.keep_shifts == []
+										# new one will create empty infor
+										emp.keep_shifts.create!(keep_shift: {date => [nil,0]})
+									end
+									# Who has infor in that date will not go to "rescue NoMethodError"
+									
+									hash_exist = emp.keep_shifts[0].keep_shift  # this is hash
+									
+									#take hash in that select date to update them or create new one
+									shifter_code_keep = hash_exist[select_date][0] 
+									ot_plan_keep = hash_exist[select_date][1]
+									
+									shifter_code = emp.shifter_code
+									ot_plan = emp.ot_plan
+									
+									change_ot = 0
+									change_shifter = ''
+									
+									puts "#{shifter_code_keep}==#{shifter_code}==#{ot_plan_keep}==#{ot_plan}"
+									
+									#XY = shifter_code case|ot case
+									if( (shifter_code != shifter_code_keep) && (ot_plan != ot_plan_keep) ) # 00
+										hash_exist[select_date] = [shifter_code, ot_plan]
+										puts "00"
+										change_ot = ot_plan
+										change_shifter = shifter_code
+										
+									elsif ( (shifter_code != shifter_code_keep) && (ot_plan == ot_plan_keep) )#01
+										hash_exist[select_date] = [shifter_code, ot_plan]
+										puts "01"
+										change_ot = ot_plan_keep
+										change_shifter = shifter_code
+										
+									elsif ( (shifter_code == shifter_code_keep) && (ot_plan != ot_plan_keep) ) #10
+										hash_exist[select_date] = [shifter_code, ot_plan]
+										puts "10"
+										change_ot = ot_plan
+										change_shifter = shifter_code_keep
+										
+									elsif ( (shifter_code == shifter_code_keep) && (ot_plan == ot_plan_keep) ) #11
+										hash_exist[select_date] = [shifter_code, ot_plan]
+										puts "11"
+										change_ot = ot_plan_keep
+										change_shifter = shifter_code_keep
+										
+									end
+									
+									puts "================== shifter_code=>#{change_shifter}, ot_plan=>#{change_ot} =================="
+									emp.keep_shifts[0].update(keep_shift: hash_exist)
+									emp.update(shifter_code: change_shifter, ot_plan: change_ot)
 						rescue NoMethodError
-							# not found date that select from calendar
-							hash_temp = emp.keep_shifts[0].keep_shift
-							hash_temp[select_date] = [nil,0]
-							emp.keep_shifts[0].update(keep_shift: hash_temp)
+									# not found date that select from calendar
+									hash_temp = emp.keep_shifts[0].keep_shift
+									hash_temp[select_date] = [nil,0]
+									emp.keep_shifts[0].update(keep_shift: hash_temp)
 						end
 		end
 		puts "END@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+=end
+    end
+    
+    def check_date_update(select_date)
+					Employee.all.each do |emp|
+								begin
+											if emp.keep_shifts == []
+												# new one will create empty infor
+												emp.keep_shifts.create!(keep_shift: {date => [nil,0]})
+											end
+											# Who has infor in that date will not go to "rescue NoMethodError"
+											
+											hash_exist = emp.keep_shifts[0].keep_shift  # this is hash
+											
+											#take hash in that select date to update them or create new one
+											shifter_code_keep = hash_exist[select_date][0] 
+											ot_plan_keep = hash_exist[select_date][1]
+											
+											shifter_code = emp.shifter_code
+											ot_plan = emp.ot_plan
+											
+											change_ot = 0
+											change_shifter = ''
+											
+											puts "#{shifter_code_keep}==#{shifter_code}==#{ot_plan_keep}==#{ot_plan}"
+											
+											#XY = shifter_code case|ot case
+											if( (shifter_code != shifter_code_keep) && (ot_plan != ot_plan_keep) ) # 00
+												hash_exist[select_date] = [shifter_code, ot_plan]
+												puts "00"
+												change_ot = ot_plan
+												change_shifter = shifter_code
+												
+											elsif ( (shifter_code != shifter_code_keep) && (ot_plan == ot_plan_keep) )#01
+												hash_exist[select_date] = [shifter_code, ot_plan]
+												puts "01"
+												change_ot = ot_plan_keep
+												change_shifter = shifter_code
+												
+											elsif ( (shifter_code == shifter_code_keep) && (ot_plan != ot_plan_keep) ) #10
+												hash_exist[select_date] = [shifter_code, ot_plan]
+												puts "10"
+												change_ot = ot_plan
+												change_shifter = shifter_code_keep
+												
+											elsif ( (shifter_code == shifter_code_keep) && (ot_plan == ot_plan_keep) ) #11
+												hash_exist[select_date] = [shifter_code, ot_plan]
+												puts "11"
+												change_ot = ot_plan_keep
+												change_shifter = shifter_code_keep
+												
+											end
+											
+											puts "================== shifter_code=>#{change_shifter}, ot_plan=>#{change_ot} =================="
+											emp.keep_shifts[0].update(keep_shift: hash_exist)
+											#emp.update(shifter_code: change_shifter, ot_plan: change_ot)
+								rescue NoMethodError
+											# not found date that select from calendar
+											hash_temp = emp.keep_shifts[0].keep_shift
+											hash_temp[select_date] = [nil,0]
+											emp.keep_shifts[0].update(keep_shift: hash_temp)
+								end
+					end
     end
     
 end
