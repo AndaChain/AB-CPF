3d0acfe4c060eedac35db1c6a8c713902e152d1c
AndaChain <andaanda789@gmail.com>
2021-11-24 17:34:38 +0700

    fix bug

--- app/controllers/manage_controller.rb
+++ app/controllers/manage_controller.rb
@@ -105,7 +105,7 @@ class ManageController < ApplicationController
 		emps.all.each do |emp|
 				if emp.keep_shifts == []
 					# new one will create empty infor
-					emp.keep_shifts.create!(keep_shift: {select_date => ["s1",0]})
+					emp.keep_shifts.create!(keep_shift: {select_date => [nil,0]})
 				end
 				begin
 					# Who has infor in that date will not go to "rescue NoMethodError"
@@ -120,7 +120,7 @@ class ManageController < ApplicationController
 				rescue NoMethodError
 					# not found date that select from calendar
 					hash_temp = emp.keep_shifts[0].keep_shift
-					hash_temp[select_date] = ["s1",0]
+					hash_temp[select_date] = [nil,0]
 					emp.keep_shifts[0].update(keep_shift: hash_temp)
 				end
 		end
@@ -130,7 +130,7 @@ class ManageController < ApplicationController
 			begin
 				if emp.keep_shifts == []
 					# new one will create empty infor
-					emp.keep_shifts.create!(keep_shift: {select_date => ["s1",0]})
+					emp.keep_shifts.create!(keep_shift: {select_date => [nil,0]})
 				end
 				# Who has infor in that date will not go to "rescue NoMethodError"
 											
@@ -181,7 +181,7 @@ class ManageController < ApplicationController
 				rescue NoMethodError
 						# not found date that select from calendar
 						hash_temp = emp.keep_shifts[0].keep_shift
-						hash_temp[select_date] = ["s1",0]
+						hash_temp[select_date] = [nil,0]
 						emp.keep_shifts[0].update(keep_shift: hash_temp)
 				end
 			
